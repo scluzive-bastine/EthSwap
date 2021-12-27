2acdab04ff94c77abd13839b3ca95cbe686a7d98
Sabastine <toshfuture@gmail.com>
2021-12-27 17:29:52 +0100

    completed buy and sell functions

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -7,7 +7,13 @@ contract EthSwap {
     Token public token;
     uint256 public rate = 100;
 
-    event TokenPurchased(
+    event TokensPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+    event TokensSold(
         address account,
         address token,
         uint256 amount,
@@ -29,6 +35,23 @@ contract EthSwap {
         token.transfer(msg.sender, tokenAmount);
 
         //Emit an event
-        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint256 _amount) public {
+        // User can't sell more tokens than they have
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        // calculate the amount of ether to redeem
+        uint256 etherAmount = _amount / rate;
+
+        // Require that ethSwap has enough ether
+        require(address(this).balance >= etherAmount);
+        // Perform sale
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        // Emit Tokens sold
+        emit TokensSold(msg.sender, address(token), _amount, rate);
     }
 }
